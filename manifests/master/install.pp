ff714917ab2f334e48af19a1320f82d8b5dfbdf8
Douglas Holt <dholt@nvidia.com>
2017-03-30 17:23:48 -0400

    clean up slurmdbd

--- manifests/master/install.pp
+++ manifests/master/install.pp
@@ -1,7 +1,4 @@
 class slurm::master::install {
-  package {'slurmdbd':
-     ensure => $slurm::params::slurm_version,
-  }
   package {'slurmctld':
      ensure => $slurm::params::slurm_version,
   }
