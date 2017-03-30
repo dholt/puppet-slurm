54a4ed69d8e5a5c328fa1235564398abbc8dc0b0
Douglas Holt <dholt@nvidia.com>
2017-03-30 17:00:20 -0400

    modify service names

--- manifests/master/service.pp
+++ manifests/master/service.pp
@@ -1,11 +1,4 @@
 class slurm::master::service {
-  service { "slurmdbd":
-           ensure     => running,
-           hasstatus  => true,
-           hasrestart => true,
-           enable     => true,
-           require    => Class["slurm::master::config"];
-  }
   service { "slurmctld":
            ensure     => running,
            hasstatus  => true,
