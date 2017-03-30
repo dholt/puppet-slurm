b7cc85b92078c8e3b1e766f13347ab9b19179131
Douglas Holt <dholt@nvidia.com>
2017-03-30 17:22:55 -0400

    clean up slurmdbd

--- manifests/slurmdb/install.pp
+++ manifests/slurmdb/install.pp
@@ -2,10 +2,7 @@ class slurm::slurmdb::install {
   package {'mysql-server':
      ensure => 'present',
   }
-  package {'slurm-slurmdbd':
-     ensure => $slurm::params::slurm_version,
-  }
-  package {'slurm-sql':
+  package {'slurmdbd':
      ensure => $slurm::params::slurm_version,
   }
 }
